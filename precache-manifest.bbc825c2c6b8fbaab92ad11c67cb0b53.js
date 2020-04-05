2dffd3747c800251e87dab36e54e6b1329981b6c
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-04-05 07:45:40 +0300

    Updates

---
+++ precache-manifest.bbc825c2c6b8fbaab92ad11c67cb0b53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f892c7d22305d8fa740f33acf82d31c",
+    "url": "/monsters-react-app/index.html"
+  },
+  {
+    "revision": "6f0dc0e4855251fa4b5a",
+    "url": "/monsters-react-app/static/css/main.ba99a5a8.chunk.css"
+  },
+  {
+    "revision": "f407d99cb0a7b662bd2b",
+    "url": "/monsters-react-app/static/js/2.ea037ea9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/monsters-react-app/static/js/2.ea037ea9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f0dc0e4855251fa4b5a",
+    "url": "/monsters-react-app/static/js/main.7d3e3ce9.chunk.js"
+  },
+  {
+    "revision": "27b3acdfb3de06519a80",
+    "url": "/monsters-react-app/static/js/runtime-main.e02a68a5.js"
+  }
+]);
